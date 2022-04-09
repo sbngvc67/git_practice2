5404c44bf9e8fdc491420c9aaade5c7c0bde470c
sbngvc67 <sbngvc67@hotmail.com>
2022-04-09 09:11:31 +0200

    public class TestCase002-3

---
+++ src/us100/TestCase002.java
@@ -0,0 +1,10 @@
+package us100;
+
+public class TestCase002 {
+    public static void main(String[] args) {
+        System.out.println("TestCase001");
+        System.out.println("TestCase002");
+        System.out.println("TestCase003");
+
+    }
+}
