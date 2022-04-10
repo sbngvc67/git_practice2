9d397511c0722458e63837a605d2707d8b0026af
sbngvc67 <sbngvc67@hotmail.com>
2022-04-10 03:15:44 +0200

    dfdfdfdfd

--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -3,8 +3,8 @@ package us100;
 public class TestCase002 {
     public static void main(String[] args) {
         System.out.println("TestCase001");
-        System.out.println("TestCase002");
-        System.out.println("TestCase0031");
+        System.out.println("TestCase002"); // dfdfdfdfdfdfdfd
+        System.out.println("TestCase003");
 
     }
 }
